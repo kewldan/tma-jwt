697484cf04094dfad7ffd461773ce7dc9ce2a625
kewldan <kewldanil1@gmail.com>
2023-12-27 02:23:33 +0300

    v1.0.3 vite fix

--- src/merchant.ts
+++ src/merchant.ts
@@ -1,6 +1,7 @@
 import {sendRequest} from "./utils";
 import {AaioResponse} from "./types";
-import {createHash} from "node:crypto";
+import Hex from 'crypto-js/enc-hex';
+import sha256 from 'crypto-js/sha256'
 
 interface PaymentInfoResponse extends AaioResponse {
     id: string;
@@ -75,7 +76,7 @@ export class Merchant {
     createPayment(amountValue: number, order_id: string, description?: string, email?: string, lang?: 'en' | 'ru', referral?: string, currency?: string): string {
         const paymentCurrency = currency || this.currency;
 
-        const sign = createHash('sha256').update([this.id, String(amountValue), paymentCurrency, this.secret, order_id].join(':')).digest('hex');
+        const sign = Hex.stringify(sha256([this.id, String(amountValue), paymentCurrency, this.secret, order_id].join(':')));
 
         const amount = String(amountValue);
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import {resolve} from 'path'
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+module.exports =  defineConfig({
     build: {lib: {entry: resolve(__dirname, 'src/index.ts'), formats: ['es']}},
     resolve: {alias: {src: resolve('src/')}},
     plugins: [
